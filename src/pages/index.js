8224760fe6c1a531f929ebd60d08aba7f4b1f0d8
ibrkkus <ibrkkus@gmail.com>
2024-11-22 07:38:49 +0300

    basic comming soon frontend

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,115 +1,80 @@
 import Image from "next/image";
 import localFont from "next/font/local";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const castoro = localFont({
+  src: "./fonts/Castoro.woff2",
+  variable: "--font-castoro",
+  weight: "400",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+const montserrat = localFont({
+  src: "./fonts/Montserrat.woff2",
+  variable: "--font-montserrat",
   weight: "100 900",
 });
 
 export default function Home() {
   return (
     <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      className={`${castoro.variable} ${montserrat.variable} min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-montserrat)] flex flex-col content-between h-full min-h-screen`}
     >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <Image
+        className="col-span-5 -z-10"
+        src="/hero_bg.png"
+        alt="Abdurrahman Gazi Erkek Yurdu binası"
+        quality={100}
+        fill
+        sizes="100vw"
+        style={{
+          objectFit: "cover",
+        }}
+        priority
+      />
+      <main className="grid grid-cols-12">
+        <div className="col-span-7 self-center">
+          <div className="font-[family-name:var(--font-castoro)] mb-5">
+            <h1 className="text-5xl mb-2 text-gold-400 tracking-wide">
+              ABDURRAHMAN GAZİ
+            </h1>
+            <h2 className="text-2xl tracking-wider text-gray-600">
+              İSLAMİ İLİMLER EĞİTİM MERKEZİ
+            </h2>
+          </div>
+
+          <div
+            className="bg-gradient-to-r from-gold-400 to-transparent w-64 mb-5"
+            style={{ height: 1 }}
+          ></div>
+
+          <p className="text-gray-700 max-w-2xl leading-7 mb-5">
+            Kur'an-ı Kerim ve Sünnet-i Seniyye ışığında ilim, amel ve ihlas ile
+            bezenmiş ümmetin geleceğini yetiştiriyoruz
+          </p>
+
+          <div>
+            <button className="bg-gold-400 px-4 py-2 text-white font-medium mr-4 hover:bg-gold-500  active:bg-gold-600 ease-out duration-200">
+              DESTEK OL
+            </button>
+            <button
+              style={{ borderWidth: 1 }}
+              className="px-4 py-2 border-gray-500 hover:bg-gray-200 active:bg-gray-400 ease-out duration-200"
+            >
+              İLETİŞİM
+            </button>
+          </div>
+        </div>
+
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          className="col-span-5"
+          src="/hero.png"
+          alt="Medrese Talebeleri ve Medresemiz"
+          width={360}
+          height={600}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <div className="text-center text-sm text-gray-500">
+        SİTEMİZ YAPIM AŞAMASINDADIR
+      </div>
     </div>
   );
 }
